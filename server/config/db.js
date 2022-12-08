8b58362216d6d19fe29580631b88e701e6f8a5a5
Uzair Ahmad <uzairahmad.dev@gmail.com>
2022-12-08 22:23:14 +0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,11 @@
 import consola from "consola";
 import mongoose from "mongoose";
-import { DB_URI } from "./index.js";
 
 const { success, error } = consola;
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(DB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     success({
       badge: true,
       message: `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold,
