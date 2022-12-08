50a508d7aa7d34215884a6b2917917a290e5ff65
Uzair Ahmad <uzairahmad.dev@gmail.com>
2022-12-08 22:24:32 +0500

    Update helperFunctions.js

--- server/utils/helperFunctions.js
+++ server/utils/helperFunctions.js
@@ -1,13 +1,11 @@
 import jwt from "jsonwebtoken";
 import lodash from "lodash";
 
-import { SECRET } from "../config/index.js";
-
 export const propertyImagesLimit = (val) => val.length <= 4;
 
 export const issueToken = async (user) => {
   const { sign } = jwt;
-  let token = await sign(user, SECRET, { expiresIn: 60 * 60 * 24 });
+  let token = await sign(user, process.env.SECRET, { expiresIn: 60 * 60 * 24 });
   return `Bearer ${token}`;
 };
 
